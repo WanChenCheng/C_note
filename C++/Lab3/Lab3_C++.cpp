67389c9ebe822da8927968796a53eafbffca5edd
Ava Cheng <152263440+WanChenCheng@users.noreply.github.com>
2025-09-15 16:53:31 +0800

    Update Lab3_C++.cpp

--- C++/Lab3/Lab3_C++.cpp
+++ C++/Lab3/Lab3_C++.cpp
@@ -1,3 +1,7 @@
+//Lab3
+//經濟四 111208094 鄭琬蓁
+
+//EXERCISE 3-1 : SIMPLE FILE INPUT PRACTICE
 #include <iostream>
 #include <fstream>
 using namespace std;
@@ -6,26 +10,64 @@ int main() {
     ifstream iff("data.txt");
     int a, b;
 
-    // 第 1 行
+    // 1
     iff >> a >> b;
     cout << "Line 1: Sum=" << a+b
          << ", Diff=" << a-b
          << ", Prod=" << a*b
          << ", Quot=" << a/b << endl;
 
-    // 第 2 行
+    // 2
     iff >> a >> b;
     cout << "Line 2: Sum=" << a+b
          << ", Diff=" << a-b
          << ", Prod=" << a*b
          << ", Quot=" << a/b << endl;
 
-    // 第 3 行
+    // 3
     iff >> a >> b;
     cout << "Line 3: Sum=" << a+b
          << ", Diff=" << a-b
          << ", Prod=" << a*b
          << ", Quot=" << a/b << endl;
+
+    return 0;
+}
+
+
+//EXERCISE 3-2 : CAFÉ RECEIPT
+#include <iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+
+int main() {
+    string productName, cashierName;
+    int quantity;
+    double unitPrice;
+
+    getline(cin, productName);
+    cin >> quantity;
+    cin >> unitPrice;
+    cin.ignore();//清掉換行
+    
+    getline(cin, cashierName);
+
+    double total = quantity * unitPrice;
+
+    //調格式
+    cout << left << setw(20) << "Item"
+         << right << setw(6) << "Qty"
+         << setw(10) << "Unit"
+         << setw(12) << "Total" << endl;
+
+    cout << left << setw(20) << productName
+         << right << setw(6) << quantity
+         << fixed << setprecision(2)
+         << setw(10) << unitPrice
+         << setw(12) << total << endl;
+
+    cout << "Cashier: " << cashierName << endl;
 
     return 0;
 }
